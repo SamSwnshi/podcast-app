d31e6b961c10c3fc930511cdae802e00c7972c4b
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-02-18 17:06:37 +0530

    database connected to monogodb and server is started

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
-console.log("Started")
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connect from "./db/config.js";
+
+
+dotenv.config();
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(cors())
+
+app.listen(port,()=>{
+    connect()
+    console.log(`Server Started at Port: ${port}`)
+})
